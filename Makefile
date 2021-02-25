7049768b31e059099faf360e011bba553d1f80b2
adria0 <adria@codecontext.io>
2021-02-25 13:51:19 +0100

    update

--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
-all: target/armv7-unknown-linux-gnueabihf/release/pyramid-player
+BINARY=target/armv7-unknown-linux-gnueabihf/release/pyramid-player
 
-target/armv7-unknown-linux-gnueabihf/release/pyramid-player:
+all: $(BINARY)
+
+clean:
+	rm $(BINARY) 
+
+$(BINARY): src/main.rs
 	docker build -t crossbuild:local .
 	cross build --release --target=armv7-unknown-linux-gnueabihf
+	scp $(BINARY) pi@$(PI):/home/pi/pyramid-player
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
+extern crate rodio;
+
 use std::io::BufReader;
 
 fn main() {
-    loop {
-        let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
-        let sink = rodio::Sink::try_new(&handle).unwrap();
+    let device = rodio::default_output_device().unwrap();
+    let sink = rodio::Sink::new(&device);
 
-        let file = std::fs::File::open("1.mp3").unwrap();
-        sink.append(rodio::Decoder::new(BufReader::new(file)).unwrap());
+    let file = std::fs::File::open("1.mp3").unwrap();
+    sink.append(rodio::Decoder::new(BufReader::new(file)).unwrap());
 
-        sink.sleep_until_end();
-    }
-
+    sink.sleep_until_end();
 }
